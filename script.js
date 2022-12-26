58483a6762ca64c038c8d5331297e3867aa791a6
Matin Tavacoli <mtavacoli33@gmail.com>
2022-12-26 12:52:03 -0600

    added button

---
+++ script.js
@@ -0,0 +1,32 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// Add event listener to the button
+
+// THEN a timer starts
+// Need to count down once every second
+// and I am presented with a question
+// Function that will show user the question
+
+// WHEN I answer a question
+// Need to add event listener to the click being used for the answer
+
+// THEN I am presented with another question
+// Function that will show the user the next question
+// WHEN I answer a question incorrectly
+// Function that gives a pop up saying Wrong!
+// THEN time is subtracted from the clock
+// An integer will be suctracted from the integer
+// WHEN all questions are answered or the timer reaches 0
+// Check to see if the integer is equal to zero
+// THEN the game is over
+// Once the clock has reached zero then the game is over
+// WHEN the game is over
+// When the lcok has reached zero or the user is out of questions
+// THEN I can save my initials and score
+// Use localStorage to save initals and score
+
+var startButton = document.querySelector("#startbtn");
+
+function test() {
+  console.log("connected");
+}
